0c91c84dc4c7192a93cc88c26bf0b36e0908de88
hya0906 <hya0906@naver.com>
2022-01-24 00:14:24 +0900

    remove print("hihihi")

--- week_4/homework/03_get_all_ways_of_theater_seat.py
+++ week_4/homework/03_get_all_ways_of_theater_seat.py
@@ -66,6 +66,5 @@ def get_all_ways_of_theater_seat(total_count, fixed_seat_array):
     all_ways *= count_of_ways
     return all_ways
 
-print("hihihi")
 # 12가 출력되어야 합니다!
 print(get_all_ways_of_theater_seat(seat_count, vip_seat_array))
